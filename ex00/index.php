a549a685bc3f516c1d5b0ff26631d0b9e0d264a5
patrícia costa <patyamiga16@hotmail.com>
2024-08-21 10:35:24 -0300

    ex00

---
+++ ex00/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu documento</title>
+</head>
+
+<body>
+
+    <h1>
+        Cabeçalho principal do meu documento
+    </h1>
+    <h2>
+        Cabeçalho secundário do meu documento
+    </h2>
+    <p>
+        Meu parágrafo contendo um texto extremamente grande.
+        Para mais informações visite
+        <a href="outro_documento.html">este outro documento</a>
+    </p>
+    <img src="cachorro.avif" alt="Cachorro">
+
+</body>
+
+</html>
